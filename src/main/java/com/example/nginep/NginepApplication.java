36637c56dcdcc50ceb7fc6dcb61e22331f50693c
JanBu <yosefsitumorang97@gmail.com>
2024-08-17 14:30:15 +0700

    build:new project nginep backend

---
+++ src/main/java/com/example/nginep/NginepApplication.java
@@ -0,0 +1,13 @@
+package com.example.nginep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NginepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NginepApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/nginep/NginepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nginep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NginepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
